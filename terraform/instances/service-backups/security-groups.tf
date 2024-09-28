6220b11792ddbeffdd0bd72faae936a884fb89a3
karlkern <karl.kern@nationalarchives.gov.uk>
2024-09-28 20:52:53 +0100

    fix cidr

--- terraform/instances/service-backups/security-groups.tf
+++ terraform/instances/service-backups/security-groups.tf
@@ -9,9 +9,7 @@ resource "aws_security_group" "service_backups" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "rp_response_ingress" {
-    cidr_blocks = [
-        var.private_subnet_cidr_block,
-    ]
+    cidr_ipv4 = var.private_subnet_cidr_block,
     description       = "allow response from internal subnets"
     from_port         = 1024
     to_port           = 65535
@@ -20,9 +18,7 @@ resource "aws_vpc_security_group_ingress_rule" "rp_response_ingress" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "rp_egress" {
-    cidr_blocks = [
-        "0.0.0.0/0"
-    ]
+    cidr_ipv4 = "0.0.0.0/0"
     from_port         = 0
     to_port           = 0
     ip_protocol       = "-1"
