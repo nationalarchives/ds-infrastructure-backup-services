8693b3605628364bc90d60a158cdf5c3bea93b84
karlkern <karl.kern@nationalarchives.gov.uk>
2024-09-29 09:09:07 +0100

    import

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -98,4 +98,7 @@ import {
   to = module.s3-tna-backup-intake.aws_s3control_access_point_policy.ap_policy["ds-backup-target"]
   id = "arn:aws:s3:eu-west-2:637423167251:accesspoint/ds-backup-target"
 }
-
+import {
+  to = module.s3-tna-backup-intake.aws_s3_bucket_policy.tna_backup_intake_access_from_another_account
+  id = "tna-backup-intake"
+}
