890427c0358e6d410e97d03eda8953c581645b82
karlkern <karl.kern@nationalarchives.gov.uk>
2024-09-29 09:23:59 +0100

    enabling  bucket key

--- terraform/s3/tna-backup-vault/tna-backup-vault.tf
+++ terraform/s3/tna-backup-vault/tna-backup-vault.tf
@@ -35,6 +35,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tna_backup_vault"
         apply_server_side_encryption_by_default {
             sse_algorithm = "AES256"
         }
+        bucket_key_enabled = true
     }
 }
 
