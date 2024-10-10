5c5055ef49172ee8be94d3467dd90e6b1684890f
karlkern <karl.kern@nationalarchives.gov.uk>
2024-10-10 13:21:42 +0100

    debug

--- terraform/sns/sns.tf
+++ terraform/sns/sns.tf
@@ -4,11 +4,11 @@ variable "topic" {}
 
 resource "aws_sns_topic" "s3_backup_check_in_topic" {
     name = "s3-backup-check-in-topic"
-    policy = templatefile(templatefile("${path.module}/templates/sns-publish-policy.json", {
-        account_id = var.account_id,
-        bucket_arn = var.bucket_arn,
-        topic = var.topic
-    }))
+    policy = templatefile("${path.module}/templates/sns-publish-policy.json", {
+        account_id = var.account_id
+        bucket_arn = var.bucket_arn
+        topic      = var.topic
+    })
 }
 
 output "s3_backup_check_in_topic_arn" {
