241dd4b98e7ad96a932ab39bfe807a909a6a811b
karlkern <karl.kern@nationalarchives.gov.uk>
2024-10-11 08:44:09 +0100

    debug

--- terraform/lambda/backup-check-in/security-groups.tf
+++ terraform/lambda/backup-check-in/security-groups.tf
@@ -21,7 +21,7 @@ resource "aws_vpc_security_group_ingress_rule" "replies" {
 resource "aws_vpc_security_group_egress_rule" "https" {
     security_group_id = aws_security_group.lambda_backup_drop_zone_sg.id
 
-    cidr_ipv4   = "0.0.0.0/8"
+    cidr_ipv4   = "0.0.0.0/0"
     description = "https calls to the world"
     from_port   = 443
     ip_protocol = "tcp"
@@ -41,7 +41,7 @@ resource "aws_vpc_security_group_egress_rule" "connect_mysql" {
 resource "aws_vpc_security_group_egress_rule" "replies" {
     security_group_id = aws_security_group.lambda_backup_drop_zone_sg.id
 
-    cidr_ipv4   = "0.0.0.0/8"
+    cidr_ipv4   = "0.0.0.0/0"
     description = "let in replies from outside"
     from_port   = 49152
     ip_protocol = "tcp"
