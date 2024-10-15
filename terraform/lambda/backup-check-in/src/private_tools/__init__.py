495bf9befd72e5fe8939ec9b1da505f7d3e9b99e
karlkern <karl.kern@nationalarchives.gov.uk>
2024-10-15 13:02:09 +0100

    refactor lambda

--- terraform/lambda/backup-check-in/src/private_tools/__init__.py
+++ terraform/lambda/backup-check-in/src/private_tools/__init__.py
@@ -1,8 +1,8 @@
-from db_mysql import Database
-from sqs import Queue
-from asm import Secrets
-from s3 import Bucket
-from helpers import find_key_dict, set_random_id, size_converter
+from .db_mysql import Database
+from .sqs import Queue
+from .asm import Secrets
+from .s3 import Bucket
+from .helpers import find_key_dict, set_random_id, size_converter
 
 __all__ = [
     "Database",
