2f90d3691013a47796267c0105cb43ef9a603f22
karlkern <karl.kern@nationalarchives.gov.uk>
2024-10-24 15:38:21 +0100

    refactoring

--- terraform/lambda/backup-check-in/src/backup-check-in.py
+++ terraform/lambda/backup-check-in/src/backup-check-in.py
@@ -39,61 +39,87 @@ def process_object(event_data):
     # find any entry already in process
     select_where = f'etag = "{object_info["ETag"]}" AND object_key = "{object_key}" AND status = 1'
     select_fields = ['id', 'etag', 'object_key']
-    check_in_db.select('object_checkins', select_fields, select_where)
-    object_data = {'bucket': bucket_name, 'object_key': object_key,
-                   'object_name': object_name, 'etag': object_info['ETag'].replace('"',''),
-                   'object_size': object_info['ContentLength'], 'object_type': object_info['ContentType'],
-                   'last_modified': object_info['LastModified'],
-                   'created_at': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'status': 2}
-    if "Metadata" in object_info:
-        obj_metadata = object_info['Metadata']
-        if find_key_dict("retention_period", obj_metadata):
-            object_data['retention_period'] = obj_metadata['retention_period']
-        if find_key_dict("lock_mode", obj_metadata):
-            object_data['lock_mode'] = obj_metadata['lock_mode']
-        if find_key_dict("legal_hold", obj_metadata):
-            object_data['legal_hold'] = obj_metadata['legal_hold']
-        if find_key_dict("lock-until-date", obj_metadata):
-            object_data['lock_until_date'] = obj_metadata['lock-until-date']
-        if 'ChecksumCRC32' in  object_info:
-            object_data['checksum_crc32'] = object_info['ChecksumCRC32']
-        if 'ChecksumCRC32C' in  object_info:
-            object_data['checksum_crc32c'] = object_info['ChecksumCRC32C']
-        if 'ChecksumSHA1' in  object_info:
-            object_data['checksum_sha1'] = object_info['ChecksumSHA1']
-        if 'ChecksumSHA256' in  object_info:
-            object_data['checksum_sha256'] = object_info['ChecksumSHA256']
-    file_id = check_in_db.insert('object_checkins', object_data)
-    object_data['file_id'] = file_id
-    size_str = str(object_info['ContentLength'])
+    found_records = check_in_db.select('object_checkins', select_fields, select_where)
+    if len(found_records) == 0:
+        object_data = {'bucket': bucket_name, 'object_key': object_key,
+                       'object_name': object_name, 'etag': object_info['ETag'].replace('"',''),
+                       'object_size': object_info['ContentLength'], 'object_type': object_info['ContentType'],
+                       'last_modified': object_info['LastModified'],
+                       'created_at': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'status': 2}
+        if "Metadata" in object_info:
+            obj_metadata = object_info['Metadata']
+            if find_key_dict("retention_period", obj_metadata):
+                object_data['retention_period'] = obj_metadata['retention_period']
+            if find_key_dict("lock_mode", obj_metadata):
+                object_data['lock_mode'] = obj_metadata['lock_mode']
+            if find_key_dict("legal_hold", obj_metadata):
+                object_data['legal_hold'] = obj_metadata['legal_hold']
+            if find_key_dict("lock-until-date", obj_metadata):
+                object_data['lock_until_date'] = obj_metadata['lock-until-date']
+            if 'ChecksumCRC32' in  object_info:
+                object_data['checksum_crc32'] = object_info['ChecksumCRC32']
+            if 'ChecksumCRC32C' in  object_info:
+                object_data['checksum_crc32c'] = object_info['ChecksumCRC32C']
+            if 'ChecksumSHA1' in  object_info:
+                object_data['checksum_sha1'] = object_info['ChecksumSHA1']
+            if 'ChecksumSHA256' in  object_info:
+                object_data['checksum_sha256'] = object_info['ChecksumSHA256']
+        file_id = check_in_db.insert('object_checkins', object_data)
+        object_data['file_id'] = file_id
+        size_str = str(object_info['ContentLength'])
 
-    queue = Queue(parameters['queue_url'])
-    sqs_body = f'''\
-    {
-        "file_id": "{file_id}"
-        "bucket": "{bucket}"
-        "object_key": "{object_key}"
-        "object_name": "{object_name}"
-        "etag": "{object_info['ETag'].replace('"','')}"
-        "object_size": "{size_str}"
-        "object_type": "{object_info['ContentType']}"
-        "last_modified": "{object_info['LastModified']}"
-        "created_at": "{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-    }
-        '''
-    sqs_results = queue.add(sqs_body, set_random_id())
-    queue_data = {'message_id': sqs_results['MessageId'], 'sequence_number': sqs_results['SequenceNumber'],
-                  'created_at': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'status': 2, 'file_id': file_id}
-    if 'MD5OfMessageBody' in sqs_results:
-        queue_data['md5_of_message_body'] = sqs_results['MD5OfMessageBody']
-    if 'MD5OfMessageAttributes' in sqs_results:
-        queue_data['md5_of_message_attributes'] = sqs_results['MD5OfMessageAttributes']
-    if 'MD5OfMessageSystemAttributes' in sqs_results:
-        queue_data['md5_of_message_system_attributes'] = sqs_results['MD5OfMessageSystemAttributes']
-    queue_id = check_in_db.insert('queues', queue_data)
-    updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    object_data = {"queue_id": queue_id, "updated_at": updated_at}
-    where_clause = 'id = ' + str(file_id)
-    check_in_db.update('object_checkins',  object_data, where_clause)
+        queue = Queue(parameters['queue_url'])
+        sqs_body = f'''\
+        {
+            "file_id": "{file_id}"
+            "bucket": "{bucket}"
+            "object_key": "{object_key}"
+            "object_name": "{object_name}"
+            "etag": "{object_info['ETag'].replace('"','')}"
+            "object_size": "{size_str}"
+            "object_type": "{object_info['ContentType']}"
+            "last_modified": "{object_info['LastModified']}"
+            "created_at": "{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        }
+            '''
+        sqs_results = queue.add(sqs_body, set_random_id())
+        queue_data = {'message_id': sqs_results['MessageId'], 'sequence_number': sqs_results['SequenceNumber'],
+                      'created_at': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'status': 2, 'file_id': file_id}
+        if 'MD5OfMessageBody' in sqs_results:
+            queue_data['md5_of_message_body'] = sqs_results['MD5OfMessageBody']
+        if 'MD5OfMessageAttributes' in sqs_results:
+            queue_data['md5_of_message_attributes'] = sqs_results['MD5OfMessageAttributes']
+        if 'MD5OfMessageSystemAttributes' in sqs_results:
+            queue_data['md5_of_message_system_attributes'] = sqs_results['MD5OfMessageSystemAttributes']
+        queue_id = check_in_db.insert('queues', queue_data)
+        updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        object_data = {"queue_id": queue_id, "updated_at": updated_at}
+        where_clause = 'id = ' + str(file_id)
+        check_in_db.update('object_checkins',  object_data, where_clause)
+    else:
+        object_data = {'bucket': bucket_name, 'object_key': object_key,
+                       'object_name': object_name, 'etag': object_info['ETag'].replace('"',''),
+                       'object_size': object_info['ContentLength'], 'object_type': object_info['ContentType'],
+                       'last_modified': object_info['LastModified'],
+                       'created_at': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'status': 7}
+        if "Metadata" in object_info:
+            obj_metadata = object_info['Metadata']
+            if find_key_dict("retention_period", obj_metadata):
+                object_data['retention_period'] = obj_metadata['retention_period']
+            if find_key_dict("lock_mode", obj_metadata):
+                object_data['lock_mode'] = obj_metadata['lock_mode']
+            if find_key_dict("legal_hold", obj_metadata):
+                object_data['legal_hold'] = obj_metadata['legal_hold']
+            if find_key_dict("lock-until-date", obj_metadata):
+                object_data['lock_until_date'] = obj_metadata['lock-until-date']
+            if 'ChecksumCRC32' in  object_info:
+                object_data['checksum_crc32'] = object_info['ChecksumCRC32']
+            if 'ChecksumCRC32C' in  object_info:
+                object_data['checksum_crc32c'] = object_info['ChecksumCRC32C']
+            if 'ChecksumSHA1' in  object_info:
+                object_data['checksum_sha1'] = object_info['ChecksumSHA1']
+            if 'ChecksumSHA256' in  object_info:
+                object_data['checksum_sha256'] = object_info['ChecksumSHA256']
+        file_id = check_in_db.insert('object_checkins', object_data)
 
     check_in_db.close()
