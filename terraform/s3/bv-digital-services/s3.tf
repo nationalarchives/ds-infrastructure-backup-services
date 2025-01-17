0e9dcc6e7d042d396f9de2412e5d80bcdd38fd7d
karlkern <karl.kern@nationalarchives.gov.uk>
2025-01-17 09:25:30 +0000

    debug

--- terraform/s3/bv-digital-services/s3.tf
+++ terraform/s3/bv-digital-services/s3.tf
@@ -5,8 +5,8 @@ variable "tna_backup_inventory_arn" {}
 # backup vault collecting backups for Digital Services
 # retention settings
 # ------------------------------------------------------------------------------
-resource "aws_s3_bucket" "bv_ds_digital_files" {
-    bucket = "bv-ds-digital-files"
+resource "aws_s3_bucket" "bv_digital_services" {
+    bucket = "bv-digital-services"
 
     force_destroy       = false
     object_lock_enabled = true
@@ -14,21 +14,21 @@ resource "aws_s3_bucket" "bv_ds_digital_files" {
     tags = merge(
         var.default_tags,
         {
-            Name = "bv-ds-digital-files"
+            Name = "bv-digital-services"
         }
     )
 }
 
-resource "aws_s3_bucket_ownership_controls" "bv_ds_digital_files" {
-    bucket = aws_s3_bucket.bv_ds_digital_files.id
+resource "aws_s3_bucket_ownership_controls" "bv_digital_services" {
+    bucket = aws_s3_bucket.bv_digital_services.id
 
     rule {
         object_ownership = "BucketOwnerEnforced"
     }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "bv_ds_digital_files" {
-    bucket = aws_s3_bucket.bv_ds_digital_files.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "bv_digital_services" {
+    bucket = aws_s3_bucket.bv_digital_services.id
 
     rule {
         apply_server_side_encryption_by_default {
@@ -38,8 +38,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bv_ds_digital_fil
     }
 }
 
-resource "aws_s3_bucket_public_access_block" "bv_ds_digital_files" {
-    bucket = aws_s3_bucket.bv_ds_digital_files.id
+resource "aws_s3_bucket_public_access_block" "bv_digital_services" {
+    bucket = aws_s3_bucket.bv_digital_services.id
 
     block_public_acls       = false
     block_public_policy     = false
@@ -47,16 +47,16 @@ resource "aws_s3_bucket_public_access_block" "bv_ds_digital_files" {
     restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_versioning" "bv_ds_digital_files" {
-    bucket = aws_s3_bucket.bv_ds_digital_files.id
+resource "aws_s3_bucket_versioning" "bv_digital_services" {
+    bucket = aws_s3_bucket.bv_digital_services.id
 
     versioning_configuration {
         status = "Enabled"
     }
 }
 
-resource "aws_s3_bucket_inventory" "bv_ds_digital_files" {
-    bucket = aws_s3_bucket.bv_ds_digital_files.id
+resource "aws_s3_bucket_inventory" "bv_digital_services" {
+    bucket = aws_s3_bucket.bv_digital_services.id
     name   = "EntireBucketWeekly"
 
     included_object_versions = "All"
