85588100955e844091a6cc713bc1f345e939f79a
karlkern <karl.kern@nationalarchives.gov.uk>
2025-01-19 21:48:19 +0000

    debug

--- terraform/instances/backup-drop-zone/instance.tf
+++ terraform/instances/backup-drop-zone/instance.tf
@@ -1,13 +1,15 @@
 resource "aws_instance" "backup_drop_zone" {
-    ami                    = var.image_id
-    instance_type          = var.instance_type
-    key_name               = var.key_name
-    iam_instance_profile   = aws_iam_instance_profile.ec2_drop_zone_profile.name
+    ami                  = var.image_id
+    instance_type        = var.instance_type
+    key_name             = var.key_name
+    iam_instance_profile = aws_iam_instance_profile.ec2_drop_zone_profile.name
     vpc_security_group_ids = [
         aws_security_group.backup_drop_zone.id,
     ]
-    subnet_id              = var.subnet_id
-    user_data              = file("${path.module}/scripts/userdata.sh")
+    subnet_id = var.subnet_id
+    user_data = templatefile("${path.module}/scripts/userdata.sh", {
+        ssm_id = "/application/lambda/backup-check-in"
+    })
 
     metadata_options {
         http_tokens            = "required"
