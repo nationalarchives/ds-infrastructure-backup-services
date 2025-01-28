053ca9eea9fad39fb1badc470f785e202490ddff
karlkern <karl.kern@nationalarchives.gov.uk>
2025-01-28 09:53:28 +0000

    debug

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -3,7 +3,7 @@ module "sgs" {
 
     vpc_id = local.vpc_info.id
     db_subnet_cidrs = [
-        local.private_subnet_a_info.cidr
+        local.private_subnet_a_info.cidr_block
     ]
 
     tags = local.default_tags
