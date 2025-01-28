859d7bb329d71dfcd963124ff0706b272cd4a958
karlkern <karl.kern@nationalarchives.gov.uk>
2025-01-28 09:50:03 +0000

    debug

--- terraform/security-groups/egress-database.tf
+++ terraform/security-groups/egress-database.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "ec2_egress_database" {
 resource "aws_vpc_security_group_egress_rule" "database_egress" {
     count = length(var.db_subnet_cidrs)
 
-    cidr_ipv4         = var.subnet_cidrs[count.index]
+    cidr_ipv4         = var.db_subnet_cidrs[count.index]
     from_port         = 3306
     ip_protocol       = "tcp"
     security_group_id = aws_security_group.ec2_egress_database.id
