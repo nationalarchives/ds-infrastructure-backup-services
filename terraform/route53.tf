7e4a24162533625a1a1c784b57e6e833169a5f52
karlkern <karl.kern@nationalarchives.gov.uk>
2025-01-28 08:39:55 +0000

    debug

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,8 +1,8 @@
-module "route53" {
-    source = "./route53"
-
-    public_domain = "tna-backup-drop-zone.nationalarchives.gov.uk"
-    load_balancer_dns_name = module.ec2-shared-lb.load_balancer_dns_name
-
-    tags = local.default_tags
-}
+#module "route53" {
+#    source = "./route53"
+#
+#    public_domain = "tna-backup-drop-zone.nationalarchives.gov.uk"
+#    load_balancer_dns_name = module.ec2-shared-lb.load_balancer_dns_name
+#
+#    tags = local.default_tags
+#}
